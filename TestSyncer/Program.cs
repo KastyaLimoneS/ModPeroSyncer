008e7796bab3c145e400fbc3b2bbf4797eda8911
KastyaLimoneSS <KastyaLimoness@yandex.ru>
2023-03-26 03:13:22 +0300

    Created simple console app to use ModSyncer

---
+++ TestSyncer/Program.cs
@@ -0,0 +1,25 @@
+﻿using SyncerLogic.main;
+
+var logic = new Logic(Environment.OSVersion.Platform.ToString());
+
+int a = 0;
+
+while (true) {
+    String[] command = Console.ReadLine().Split(" ");
+    if (command[0] == "list") {
+        Console.WriteLine("Your Packs:");
+        foreach(var pack in logic.GetPackNames()) Console.WriteLine("|-"+pack);
+    }
+    if (command[0] == "add") {
+        logic.AddPack(command[1], command[2]);
+        Console.WriteLine($"Pack \"{command[1]}\" was added with repo \"{command[2]}\"");
+    }
+    if (command[0] == "update") {
+        Console.WriteLine();
+        foreach(var stage in logic.UpdateMods(command[1], command[2]))
+            {Console.Write("###"); a+=stage;}
+        Console.WriteLine("\n Your mods are updated");
+    }
+    if (command[0] == "exit")
+        break;
+}
